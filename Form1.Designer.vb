cf77c6716079a362bbb0475ea0859113a50b4554
Yakub Kristianto <yakub@rumah.com>
2014-02-27 13:37:31 +0700

    First initial work

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(252, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(252, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(234, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(12, 70)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(315, 227)
+        Me.TextBox2.TabIndex = 4
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.IncludeSubdirectories = True
+        Me.FileSystemWatcher1.NotifyFilter = System.IO.NotifyFilters.LastWrite
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 250
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(339, 309)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.MinimumSize = New System.Drawing.Size(250, 250)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents FileSystemWatcher1 As System.IO.FileSystemWatcher
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+
+Public Class Form1
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        FileSystemWatcher1.EnableRaisingEvents = False
+        Button1.Enabled = True
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Control.CheckForIllegalCrossThreadCalls = False
+        'watchfolder = New System.IO.FileSystemWatcher()
+
+        'this is the path we want to monitor
+        FileSystemWatcher1.Path = TextBox1.Text()
+
+        'Add a list of Filter we want to specify
+        'make sure you use OR for each Filter as we need to
+        'all of those 
+
+        'FileSystemWatcher1.NotifyFilter = IO.NotifyFilters.DirectoryName
+        'FileSystemWatcher1.NotifyFilter = watchfolder.NotifyFilter Or IO.NotifyFilters.FileName
+        'FileSystemWatcher1.NotifyFilter = watchfolder.NotifyFilter Or IO.NotifyFilters.Attributes
+
+
+        ' add the handler to each event
+        'AddHandler watchfolder.Changed, AddressOf logchange
+        'AddHandler watchfolder.Created, AddressOf logchange
+        'AddHandler watchfolder.Deleted, AddressOf logchange
+
+        ' add the rename handler as the signature is different
+        'AddHandler watchfolder.Renamed, AddressOf logrename
+
+        'Set this property to true to start watching
+        FileSystemWatcher1.EnableRaisingEvents = True
+
+        Button1.Enabled = False
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub FileSystemWatcher1_Changed(ByVal sender As Object, ByVal e As System.IO.FileSystemEventArgs) Handles FileSystemWatcher1.Changed, FileSystemWatcher1.Created, FileSystemWatcher1.Deleted
+        If Timer1.Enabled Then Exit Sub
+        Timer1.Enabled = True
+        If e.ChangeType = IO.WatcherChangeTypes.Changed Then
+            TextBox2.Text &= "File " & e.FullPath & " has been modified" & vbCrLf
+        End If
+        If e.ChangeType = IO.WatcherChangeTypes.Created Then
+            TextBox2.Text &= "File " & e.FullPath & " has been created" & vbCrLf
+        End If
+        If e.ChangeType = IO.WatcherChangeTypes.Deleted Then
+            TextBox2.Text &= "File " & e.FullPath & " has been deleted" & vbCrLf
+        End If
+    End Sub
+
+    Private Sub FileSystemWatcher1_Renamed(ByVal sender As Object, ByVal e As System.IO.RenamedEventArgs) Handles FileSystemWatcher1.Renamed
+        If Timer1.Enabled Then Exit Sub
+        Timer1.Enabled = True
+        TextBox2.Text &= "File" & e.OldName & " has been renamed to " & e.Name & vbCrLf
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = False
+    End Sub
+End Class
