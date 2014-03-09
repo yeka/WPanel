123bfa85b5a1f18e25e003ae01f306245e485d1d
Yakub Kristianto <yakub1986@gmail.com>
2014-03-09 09:26:23 +0700

    Single instance

--- WPanel/Program.cs
+++ WPanel/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WindowsFormsApplication1
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "WPanel - yk");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             AppDomain.CurrentDomain.AppendPrivatePath(Application.StartupPath + "\\lib");
